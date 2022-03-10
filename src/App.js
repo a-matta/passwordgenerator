65aee14239759b07bf7652ce8d15497d51ae14de
Amrita Matta <matta.amrita@gmail.com>
2022-03-10 23:18:34 +0200

    added skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator-header">Password Generator</h2>
+          <div className="generate-password">
+            <h3>Password</h3>
+            <button className="copy_btn">
+              <i class="fa-solid fa-clipboard-list"></i>
+            </button>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password-strength">Password Strength</label>
+            <input
+              type="number"
+              name="password-strength"
+              id="password-strength"
+              max="20"
+              min="10"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Include UpperCase Letters</label>
+            <input
+              type="checkbox"
+              name="uppercase-letters"
+              id="uppercase-letters"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Include Lowercase Letters</label>
+            <input
+              type="checkbox"
+              name="lowercase-letters"
+              id="lowercase-letters"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input
+              type="checkbox"
+              name="include-numbers"
+              id="include-numbers"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include symbols</label>
+            <input
+              type="checkbox"
+              name="include-symbols"
+              id="include-symbols"
+            />
+          </div>
+          <button className="generator__btn">Generate Button </button>
+        </div>
+      </div>
     </div>
   );
 }
